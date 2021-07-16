2d4967e50d5465bd738b8f9b4023dc0347c73be9
caturprasatya <caturprasatya1224@gmail.com>
2021-07-16 20:59:49 +0700

    use package resize when upload items

--- src/components/PopularWork/InputForm.vue
+++ src/components/PopularWork/InputForm.vue
@@ -11,45 +11,54 @@
      </blockquote>
     </div>
     <div v-else class="sm:max-w-lg w-full p-10 bg-gray-300 shadow rounded-xl z-10">
-    <div class="text-center">
-      <h2 class="mt-5 text-3xl font-bold text-gray-900">
-        File Upload!
-      </h2>
-      <p class="mt-2 text-sm text-gray-400">Lorem ipsum is placeholder text.</p>
-    </div>
+      <div class="text-center">
+        <h2 class="mt-5 text-3xl font-bold text-gray-900">
+          File Upload!
+        </h2>
+        <p class="mt-2 text-sm text-gray-400">Lorem ipsum is placeholder text.</p>
+      </div>
       <form class="mt-8 space-y-3" action="#" method="POST">
         <div class="grid grid-cols-1 space-y-2">
-            <label class="text-sm font-bold text-gray-500 tracking-wide">Title</label>
-                <input class="text-base p-2 border bg-gray-200 border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500" type="" placeholder="mail@gmail.com">
+          <label class="text-sm font-bold text-gray-500 tracking-wide">Title</label>
+            <input class="text-base p-2 border bg-gray-200 border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500" type="" placeholder="mail@gmail.com">
         </div>
         <div class="grid grid-cols-1 space-y-2">
           <label class="text-sm font-bold text-gray-500 tracking-wide">Description</label>
-              <textarea class="text-base p-2 bg-gray-200 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500" type="" placeholder="mail@gmail.com" />
+            <textarea class="text-base p-2 bg-gray-200 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500" type="" placeholder="mail@gmail.com" />
         </div>
         <div class="grid grid-cols-1 space-y-2">
-          <label class="text-sm font-bold text-gray-500 tracking-wide">Attach Document</label>
+          <label class="text-sm font-bold text-gray-500 tracking-wide">Attach Files</label>
           <div class="flex items-center justify-center w-full">
               <div v-if="type === 'editPage'" class="absolute top-0 right-0 -mt-3 mr-3">
                   <div class="rounded-full bg-indigo-500 text-white text-xs py-1 pl-2 pr-3 leading-none"><i class="mdi mdi-fire text-base align-middle"></i> <span class="align-middle">Delete</span></div>
               </div>
               <label class="flex flex-col rounded-lg border-4 border-dashed w-full h-60 p-10 group text-center">
                   <div class="h-full w-full text-center flex flex-col justify-center items-center  ">
-                      <div class="flex flex-auto max-h-48 w-2/5 mx-auto -mt-10">
-                      <img class="has-mask h-36 object-center"
-                      :src="file.image_url" alt="freepik image">
-                      </div>
-                      <p class="pointer-none text-gray-500 "><span class="text-sm">Drag and drop</span> files here <br /> or <a @click="onPickFile" id="" class="text-blue-600 hover:underline">select a file</a> from your computer</p>
+                <ImageUplaoder
+                  :preview="true"
+                  :className="['fileinput', { 'fileinput--loaded': hasImage }]"
+                  capture="environment"
+                  :debug="1"
+                  doNotResize="gif"
+                  :autoRotate="true"
+                  outputFormat="verbose"
+                  @input="setImage"
+                  :maxHeight="300"
+                  :maxWidth="300"
+                >
+                  <label
+                  ref="input"
+                  class="flex flex-auto max-h-48 w-2/5 mx-auto -mt-10">
+                    <img
+                      class="has-mask h-36 object-center"
+                      src="https://img.freepik.com/free-vector/image-upload-concept-landing-page_52683-27130.jpg?size=338&ext=jpg" alt="freepik image">
+                  </label>
+                </ImageUplaoder>
                   </div>
-                  <input type="file"
-                   @change="previewImage"
-                   class="hidden"
-                   ref="input" />
               </label>
           </div>
         </div>
-          <p class="text-sm text-gray-300">
-              <span>File type: types of videos and types of images {{}} </span>
-          </p>
+          <p class="pointer-none text-gray-500 "><span class="text-sm">Drag and drop</span> files here <br /> or <a @click="onPickFile" id="" class="text-blue-600 hover:underline">select a file</a> from your computer</p>
         <div>
           <button type="submit" class="my-5 w-full flex justify-center bg-blue-500 text-gray-100 p-4  rounded-full tracking-wide
             font-semibold  focus:outline-none focus:shadow-outline hover:bg-blue-600 shadow-lg cursor-pointer transition ease-in duration-300">
@@ -64,6 +73,7 @@
 <script>
 import { mapState } from 'vuex'
 import { storage } from '../../configs/firebase'
+import ImageUplaoder from 'vue-image-upload-resize'
 
 export default {
   name: 'InputForm',
@@ -78,10 +88,18 @@ export default {
           : 'https://img.freepik.com/free-vector/image-upload-concept-landing-page_52683-27130.jpg?size=338&ext=jpg'
       },
       file_data: null,
-      progressBar: 0
+      progressBar: 0,
+      hasImage: false,
+      image: null
     }
   },
   methods: {
+    setImage: function (output) {
+      this.hasImage = true
+      this.image = output
+      console.log('Info', output.info)
+      console.log('Exif', output.exif)
+    },
     onUpload () {
       const uploadTask = storage.ref(`media/${this.file_data?.name}`).put(this.file_data)
       uploadTask.on(
@@ -131,6 +149,9 @@ export default {
   },
   computed: {
     ...mapState(['data'])
+  },
+  components: {
+    ImageUplaoder
   }
 }
 </script>
@@ -139,5 +160,8 @@ export default {
   .has-mask {
     position: absolute;
     clip: rect(10px, 150px, 130px, 10px);
+  }
+  #fileInput {
+    display: none;
   }
 </style>
